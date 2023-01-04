8279752b3f2add210ad3b494fa4a293dd8276927
seymennklc <seymennklc@gmail.com>
2023-01-04 12:02:54 +0300

    initial commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+// chakra
+import { ColorModeScript } from '@chakra-ui/react';
+import theme from '@/theme';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
