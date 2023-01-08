8b6a929115af035cd9ef13ef08da6b74a2d1b351
seymennklc <seymennklc@gmail.com>
2023-01-09 00:17:34 +0300

    feat: add functionality of reading from mdx files

---
+++ utils/project.ts
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const rootDir = path.join(process.cwd(), 'content', 'projects');
+
+const getAllProjects = () => {
+   return fs.readdirSync(rootDir);
+};
+
+const getProjectMeta = (currentSlug: string) => {
+   const slug = currentSlug.replace(/\.mdx$/, '');
+   const filePath = path.join(rootDir, `${slug}.mdx`);
+   const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' });
+
+   const { data } = matter(fileContent);
+
+   return {
+      meta: { ...data, slug },
+   };
+};
+
+export const getAllProjectsMeta = () => {
+   return getAllProjects().map(file => getProjectMeta(file));
+};
+
+export const GetAllSlugs = () => {
+   return getAllProjects().map(file => file.replace(/\.mdx$/, ''));
+};
